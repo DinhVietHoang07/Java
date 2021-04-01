32a8a25e25b25413d24eb3d804b1d62a88d8c95b
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2021-04-01 18:40:16 +0700

    Ohh Noo

---
+++ Lab 01/Ex.java
@@ -0,0 +1,6 @@
+package session01;
+
+import java.util.Scanner;
+public class Ex {
+
+}
---
+++ Lab 01/One.java
@@ -0,0 +1,10 @@
+public class One {
+    /*
+    * First Java program, which says hello.
+    */
+    public class Hello { //Save as "Hello.java"
+        public  void main(String[] args) { // Program entry point
+            System.out.println("Hello, world!"); // Print text message
+        }
+    }}
+
