947a4bf5d3d47d91a1bd4521a788723f3917f772
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2021-04-08 21:25:59 +0700

    ...

---
+++ Lab 03/Session11/Account.java
@@ -0,0 +1,63 @@
+package Session11;
+
+public class Account {
+    private String id;
+    private String name;
+    private int balance;
+
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        }
+        else {
+            System.out.println("amount exceeded");
+        }
+        return balance;
+    }
+
+    public int transferTo(Account another, int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            another.balance += amount;
+        }
+        else {
+            System.out.println("amount exceeded");
+        }
+        return balance;
+    }
+
+    public String toString() {
+        return "Account[" + id + "," + name + "," + balance + "]\n";
+    }
+}
+
+
+
